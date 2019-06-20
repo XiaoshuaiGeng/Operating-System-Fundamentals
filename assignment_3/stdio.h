74e961fa051780c5731c58ce469f53cef350d64b
Xiaoshuai Geng <geng115@uwindsor.ca>
2019-06-20 01:26:10 -0400

    Update stdio.h

--- assignment_3/stdio.h
+++ assignment_3/stdio.h
@@ -1,37 +1,43 @@
 #ifndef STDIO_H
 #define STDIO_H
-#include "string.h"
+
 #define BUFF_SIZE 128
 
 typedef struct {
-	int fd;	//file descriptor
-	int *ptr //file cursor
-	char buf[BUFF_SIZE];	//file buffer
+        int fd; //file descriptor
+        int pos; //(file cursor) indicating the current file cursor position
+        char buf[BUFF_SIZE];    //file buffer
+        int buf_pos;//indicating the current buffer position
 }FILE;
 
 
-FILE* fopen(const char* filepath, const char* mode){
 //INPUT: filepath of the file, open mode
 //OUTPUT: return a FILE ptr has the file descriptor of the file
 
 //open the file path using open
 //open mode is given by char* mode
-//
-	File *tmp;
-	temp->fd = open(...)
-	return tmp
-}
+FILE* fopen(const char* filepath, const char mode);
+/*
+        File *tmp;
+        tmp->position = 0;
+        tmp->fd = open(...)
+        return tmp;
+*/
 
-fprintf(FILE *fp, char *format,..){
-	if(fp not exist){
-		return -1;
-	}
-	if(ptr += strlen(format) < BUFF_SIZE){
-		buf = format;
-	}else{
-		write(1,buf,128)
-		ptr = 0;
-		memset(buf,'\0',128);
-	}
-	return strlen(format);
-}
+void fclose(FILE *fp);
+
+void fprintf(FILE *fp, const char *s, ...);
+/*      if(fp not exist){
+                return -1;
+        }
+        if(ptr += strlen(format) < BUFF_SIZE){
+                buf = format;
+        }else{
+                write(1,buf,128)
+                ptr = 0;
+                memset(buf,'\0',128);
+        }
+        return strlen(format);
+*/
+
+void myprintf(const char* s, ...);
