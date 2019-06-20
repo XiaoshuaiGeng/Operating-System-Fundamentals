7a692e85448d2f849c370e9b60f0afa569cedbcc
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-06-19 22:26:28 -0400

    Add files via upload

--- assignment_3/stdio.h
+++ assignment_3/stdio.h
@@ -5,25 +5,29 @@
 
 typedef struct {
 	int fd;	//file descriptor
-	int pos; //file cursor
+	int position //(file cursor) indicating the current file cursor position
 	char buf[BUFF_SIZE];	//file buffer
+	int buf_pos;//indicating the current buffer position
 }FILE;
 
 
-FILE* fopen(const char* filepath, const char* mode){
 //INPUT: filepath of the file, open mode
 //OUTPUT: return a FILE ptr has the file descriptor of the file
 
 //open the file path using open
 //open mode is given by char* mode
-//
+FILE* fopen(const char* filepath, const char mode);
+/*
 	File *tmp;
-	temp->fd = open(...)
-	return tmp
-}
+	tmp->position = 0;
+	tmp->fd = open(...)
+	return tmp;
+*/
 
-fprintf(FILE *fp, char *format,..){
-	if(fp not exist){
+void fclose(FILE *fp);
+
+void fprintf(FILE *fp, char *format);
+/*	if(fp not exist){
 		return -1;
 	}
 	if(ptr += strlen(format) < BUFF_SIZE){
@@ -34,4 +38,7 @@ fprintf(FILE *fp, char *format,..){
 		memset(buf,'\0',128);
 	}
 	return strlen(format);
-}
+*/
+
+void myprintf(const char* s);
+
