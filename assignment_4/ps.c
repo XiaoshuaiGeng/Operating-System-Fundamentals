c2b536016723a748300501a01e937e6f5d88336c
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-07-01 17:47:33 -0400

    Modified files

---
+++ assignment_4/ps.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "x86.h"
+#include "proc.h"
+#include "spinlock.h"
+#include "defs.h"
+#include "syscall.h"
+
+int main(){
+	ps();
+	exit();
+}
