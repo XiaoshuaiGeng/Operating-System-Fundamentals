804bb9e32830081d7bab86edf8bf8a95da3b420f
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-07-26 19:45:04 -0400

    Add files via upload

--- assignment_5/test.c
+++ assignment_5/test.c
@@ -3,20 +3,20 @@
 #include "date.h"
 int main (int argc, char *argv[])
 {
-	int id;
+	//int id;
 	int b = 0;
 
 
-	id = fork();
-	if(id == 0)
-	{
+	//id = fork();
+	//if(id == 0)
+	//{
 		for ( int a= 0; a< 3000000; a+= 0.000001 ){
 			 b+=12345*30;
 		}
 		exit();
 
-	}else{
-		exit();
-	}
-	exit();
+	//}else{
+		//exit();
+	//}
+	//exit();
 }
